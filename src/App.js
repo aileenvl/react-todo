3ab3286682dfd4838e694daf92ccd0b820cd3e21
aileenvl <aileenvl@gmail.com>
2017-02-01 12:42:36 -0600

    binding input value to component state

--- src/App.js
+++ src/App.js
@@ -3,38 +3,45 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+    constructor(){
+        super()
+        this.state= {
+            todos:[
+                {
+                    id:1, name: 'Learn JSX',isComplete:true
+                },
+                 {
+                    id:2, name: 'Build an awesome App',isComplete:false
+                },
+                 {
+                    id:3, name: 'Ship It!',isComplete:false
+                }
+            ],
+            currentTodo:''
+        }
+        this.handleInputChange =this.handleInputChange.bind(this)
+    }
+    handleInputChange(evt){
+        this.setState({
+            currentTodo:   evt.target.value 
+        })
+      
+    }
     render() {
-        return ( <
-            div className = "App" >
-            <
-            div className = "App-header" >
-            <
-            img src = { logo }
+        return ( <div className = "App">
+         <div className = "App-header"> < img src = {logo}
             className = "App-logo"
-            alt = "logo" / >
-            <
-            h2 > React Todos < /h2> <
-            /div> <
-            div className = "Todo-App" >
-            <
-            form >
-            <
-            input type = "text" / >
-            <
-            /form> <
-            div className = "Todo-List" >
-            <
-            ul >
-            <
-            li > < input type = "checkbox" / > Learn JSX < /li> <
-            li > < input type = "checkbox" / > Build an awesome App < /li> <
-            li > < input type = "checkbox" / > Ship It! < /li>
+            alt = "logo"/> <h2> React Todos </h2> </div>
+            <div className = "Todo-App">
+            <form > < input type = "text" onChange={this.handleInputChange} value={this.state.currentTodo}/> </form> 
+            <div className = "Todo-List">
+            <ul>
+            {this.state.todos.map(todo => <li key={todo.id}> < input type="checkbox" checked={todo.isComplete}/> {todo.name}</li>)}
 
-            <
-            /ul> <
-            /div> <
-            /div> <
-            /div>
+            </ul>
+             </div>
+              </div>
+               </div>
         );
     }
 }
